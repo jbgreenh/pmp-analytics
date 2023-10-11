a78ac85710e2654092aca3b1ea09706197c15964
Jake Greenhalgh <48000025+jbgreenh@users.noreply.github.com>
2023-10-11 14:51:48 -0700

    initial commit

---
+++ test.py
@@ -0,0 +1,14 @@
+import toml
+
+with open('secrets_test.toml', 'r') as f:
+    secrets = toml.load(f)
+
+print(secrets['secret']['one'])
+print(secrets['secret']['two'])
+print('yes' if secrets['secret']['three'] else 'no')
+
+secrets['secret 2'] = {}    # empty dict, errors without this line when adding new section, don't need when section already exitsts
+secrets['secret 2']['four'] = 'secret 2 four update'   # dict entry
+
+with open('secrets_test.toml', 'w') as f:
+    toml.dump(secrets, f)
