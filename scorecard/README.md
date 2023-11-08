e8279507a3b1fce88d81f30fefe9acbc24515526
Jake Greenhalgh <48000025+jbgreenh@users.noreply.github.com>
2023-11-08 16:21:40 -0700

    added cloud_functions version and readme

---
+++ scorecard/README.md
@@ -0,0 +1,3 @@
+# scorecard  
+`scorecard.py` is for running on a local machine as needed  
+`cloud_functions_scorecard.py` is a cloud function and scheduled to run `0 10 12 * *`
--- scorecard/cloud_functions_scorecard.py
+++ scorecard/cloud_functions_scorecard.py
@@ -1,9 +1,11 @@
+import base64
 import polars as pl
 import datetime
-import toml
+import os
 
 from googleapiclient.discovery import build
-from utils import auth, drive
+import google.auth
+import drive
 
 def pull_files(service, last_month):
     '''
@@ -14,11 +16,11 @@ def pull_files(service, last_month):
     file_name = f'AZ_Dispensations_{lm_yr}{lm_mo}.csv'
     ob_file_name = f'AZ_Dispensations_{lm_yr}{lm_mo}_opioid_benzo.csv'
 
-    folder_id = secrets['folders']['dispensations_47']
+    folder_id = os.environ.get('dispensations_47', 'environment variable is not set')
     disp = drive.lazyframe_from_file_name_csv(service=service, file_name=file_name, folder_id=folder_id, sep='|')
     ob_disp = drive.lazyframe_from_file_name_csv(service=service, file_name=ob_file_name, folder_id=folder_id, sep='|')
 
-    requests_folder_id = secrets['folders']['patient_requests']
+    requests_folder_id = os.environ.get('patient_requests', 'environment variable is not set')
     requests_folder_id = drive.folder_id_from_name(service=service, folder_name=f'AZ_PtReqByProfile_{lm_yr}{lm_mo}', parent_id=requests_folder_id)
     requests = drive.lazyframe_from_file_name_csv(service=service, file_name='Prescriber.csv', folder_id=requests_folder_id, sep='|')
 
@@ -72,7 +74,7 @@ def scorecard_new_row(service, last_month):
 
 
 def update_scorecard_sheet(creds, new_row):
-    sheet_id = secrets['files']['scorecard']
+    sheet_id = os.environ.get('scorecard', 'environment variable is not set')
     range_name = 'scorecard!A:A'
     service = build('sheets', 'v4', credentials=creds)
     result = service.spreadsheets().values().get(spreadsheetId=sheet_id, range=range_name).execute()
@@ -96,16 +98,22 @@ def update_scorecard_sheet(creds, new_row):
     sheet_link = f'https://docs.google.com/spreadsheets/d/{sheet_id}'
     print(f'updated scorecard tracking: {sheet_link}')
 
-
-if __name__ == '__main__':
-    with open('../secrets.toml', 'r') as f:
-        secrets = toml.load(f)
-
+def scorecard():
     today = datetime.datetime.now()
     last_month = today.replace(day=1) - datetime.timedelta(days=1)
 
-    creds = auth.auth()
+    creds, _proj_id = google.auth.default()
     service = build('drive', 'v3', credentials=creds)
     
     new_row = scorecard_new_row(service, last_month)
     update_scorecard_sheet(creds, new_row)
+
+def hello_pubsub(event, context):
+    """Triggered from a message on a Cloud Pub/Sub topic.
+    Args:
+         event (dict): Event payload.
+         context (google.cloud.functions.Context): Metadata for the event.
+    """
+    pubsub_message = base64.b64decode(event['data']).decode('utf-8')
+    print(pubsub_message)
+    scorecard()
