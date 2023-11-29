d333245c064119ac06ae74bb4bb84377751f522f
Jake Greenhalgh <48000025+jbgreenh@users.noreply.github.com>
2023-11-29 09:30:35 -0700

    fixed filename

--- exclude_ndcs/data/README.md
+++ exclude_ndcs/data/README.md
@@ -1,3 +1,3 @@
 ## required files
 `excluded_ndcs.csv`  
-`opiate_antagonist_data.csv`
+`opiate_antagonists_data.csv`
--- exclude_ndcs/exclude_ndcs.py
+++ exclude_ndcs/exclude_ndcs.py
@@ -2,7 +2,7 @@ import polars as pl
 
 excluded_ndcs = pl.scan_csv('data/excluded_ndcs.csv', infer_schema_length=0) # infer_schema_length=0 forces all types to utf-8 and maintains the leading 0s in NDCs
 antagonists = (
-    pl.scan_csv('data/opiate_antagonist_data.csv', infer_schema_length=0)
+    pl.scan_csv('data/opiate_antagonists_data.csv', infer_schema_length=0)
     .join(excluded_ndcs, on='NDC', how='anti')
     .rename(
         {'Generic Name':'drug'}
