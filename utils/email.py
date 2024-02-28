f777453063272ffa56b90873468590c90537897f
jake greenhalgh <1jake.g@gmail.com>
2024-02-28 13:04:48 -0700

    made attachments optional

--- utils/email.py
+++ utils/email.py
@@ -7,7 +7,7 @@ from email.mime.base import MIMEBase
 from email import encoders
 from typing import Dict, List, Optional
 
-def create_message_with_attachments(sender: str, to: str, subject: str, message_text: str, file_paths: List[str], bcc: Optional[List[str]] = None) -> Dict[str, str]:
+def create_message_with_attachments(sender: str, to: str, subject: str, message_text: str, file_paths: Optional[List[str]] = None, bcc: Optional[List[str]] = None) -> Dict[str, str]:
     '''
     returns an email message with the provided sender, to, subject, message_text, and attachments at the file_paths
     '''
@@ -22,17 +22,18 @@ def create_message_with_attachments(sender: str, to: str, subject: str, message_
     msg = MIMEText(message_text)
     message.attach(msg)
 
-    for file_path in file_paths:
-        content_type, encoding = mimetypes.guess_type(file_path)
-        if content_type is None or encoding is not None:
-            content_type = 'application/octet-stream'
-        main_type, sub_type = content_type.split('/', 1)
-        with open(file_path, 'rb') as file:
-            msg = MIMEBase(main_type, sub_type)
-            msg.set_payload(file.read())
-        encoders.encode_base64(msg)
-        msg.add_header('Content-Disposition', f'attachment; filename={os.path.basename(file_path)}')
-        message.attach(msg)
+    if file_paths:
+        for file_path in file_paths:
+            content_type, encoding = mimetypes.guess_type(file_path)
+            if content_type is None or encoding is not None:
+                content_type = 'application/octet-stream'
+            main_type, sub_type = content_type.split('/', 1)
+            with open(file_path, 'rb') as file:
+                msg = MIMEBase(main_type, sub_type)
+                msg.set_payload(file.read())
+            encoders.encode_base64(msg)
+            msg.add_header('Content-Disposition', f'attachment; filename={os.path.basename(file_path)}')
+            message.attach(msg)
 
     return {'raw': base64.urlsafe_b64encode(message.as_bytes()).decode()}
 
