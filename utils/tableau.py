3c5116a57ed9ce50de13509e926b614fb4ba7925
jake greenhalgh <1jake.g@gmail.com>
2024-06-12 09:39:46 -0700

    updated type hints and doc string

--- utils/tableau.py
+++ utils/tableau.py
@@ -1,11 +1,27 @@
-import tableauserverclient as TSC
+from io import BytesIO
+from typing import Any
+
 import polars as pl
 import toml
-from io import BytesIO
-from typing import Optional
+from tableauserverclient.models.tableau_auth import PersonalAccessTokenAuth
+from tableauserverclient.server.pager import Pager
+from tableauserverclient.server.request_options import CSVRequestOptions
+from tableauserverclient.server.server import Server
 
 
-def lazyframe_from_view_id(view_id:str, filters:Optional[dict]=None, **kwargs) -> pl.LazyFrame: 
+def lazyframe_from_view_id(view_id:str, filters:dict|None=None, **kwargs:Any) -> pl.LazyFrame:
+    """
+    pulls a lazyframe from the specified view in tableau    
+
+    Args:
+        view_id: a string, luid of the target view, can be found with `find_luid()`
+        filters: optional filters to apply before pulling the lazyframe
+        kwargs:  optional kwargs to pass to polars `read_csv()`
+
+    Returns:
+        a LazyFrame containing the data from the specified view, filtered if 
+        filters are specified 
+    """
     with open('../secrets.toml', 'r') as f:
         secrets = toml.load(f)
 
@@ -14,12 +30,12 @@ def lazyframe_from_view_id(view_id:str, filters:Optional[dict]=None, **kwargs) -
     token_name = secrets['tableau']['token_name']
     token_value = secrets['tableau']['token_value']
 
-    tableau_auth = TSC.PersonalAccessTokenAuth(token_name, token_value, site)
-    tableau_server = TSC.Server(server, use_server_version=True, http_options={'verify':False})
+    tableau_auth = PersonalAccessTokenAuth(token_name, token_value, site)
+    tableau_server = Server(server, use_server_version=True, http_options={'verify':False})
 
     with tableau_server.auth.sign_in(tableau_auth):
         if filters:
-            options = TSC.CSVRequestOptions()
+            options = CSVRequestOptions()
             for k,v in filters.items():
                 options.vf(k,v)
         else:
@@ -40,11 +56,11 @@ def find_view_luid(view_name:str, workbook_name:str) -> str:
     token_name = secrets['tableau']['token_name']
     token_value = secrets['tableau']['token_value']
 
-    tableau_auth = TSC.PersonalAccessTokenAuth(token_name, token_value, site)
-    tableau_server = TSC.Server(server, use_server_version=True, http_options={'verify':False})
-    
+    tableau_auth = PersonalAccessTokenAuth(token_name, token_value, site)
+    tableau_server = Server(server, use_server_version=True, http_options={'verify':False})
+
     with tableau_server.auth.sign_in(tableau_auth):
-        all_workbooks = list(TSC.Pager(tableau_server.workbooks))
+        all_workbooks = list(Pager(tableau_server.workbooks))
         searched_workbook = [workbook for workbook in all_workbooks if workbook.name==workbook_name][0]
         tableau_server.workbooks.populate_views(searched_workbook)
         views = searched_workbook.views
