fe9bc5e17a2f7d63317198c1d07cfaf1dec94801
jbgreenh <jbgreenh@asu.edu>
2024-07-22 11:51:53 -0700

    updated drive utils

--- utils/drive.py
+++ utils/drive.py
@@ -1,9 +1,12 @@
-import polars as pl
-import io
 import datetime
+import io
+import sys
+
+import polars as pl
 import toml
 from googleapiclient.errors import HttpError
-from googleapiclient.http import MediaIoBaseDownload, MediaFileUpload
+from googleapiclient.http import MediaFileUpload, MediaIoBaseDownload
+
 
 def lazyframe_from_file_name_csv(service, file_name:str, folder_id:str, **kwargs) -> pl.LazyFrame | None:
     '''
@@ -19,11 +22,9 @@ def lazyframe_from_file_name_csv(service, file_name:str, folder_id:str, **kwargs
             try:
                 request = service.files().get_media(fileId=file_id)
             except HttpError as error:
-                print(f'error checking google drive: {error}')
-                return
+                sys.exit(f'error checking google drive: {error}')
         else:
-            print('no file found')
-            return
+            sys.exit('no file found')
 
         file = io.BytesIO()
         downloader = MediaIoBaseDownload(file, request)
@@ -33,8 +34,7 @@ def lazyframe_from_file_name_csv(service, file_name:str, folder_id:str, **kwargs
         while done is False:
             _status, done = downloader.next_chunk()
     except HttpError as error:
-        print(f'google drive error: {error}')
-        return
+        sys.exit(f'google drive error: {error}')
 
     file.seek(0) # after writing, pointer is at the end of the stream
     return pl.read_csv(file, **kwargs).lazy()
@@ -54,11 +54,9 @@ def lazyframe_from_file_name_sheet(service, file_name:str, folder_id:str, **kwar
             try:
                 request = service.files().export_media(fileId=file_id, mimeType='text/csv')
             except HttpError as error:
-                print(f'error checking google drive: {error}')
-                return
+                sys.exit(f'error checking google drive: {error}')
         else:
-            print('no file found')
-            return
+            sys.exit('no file found')
 
         file = io.BytesIO()
         downloader = MediaIoBaseDownload(file, request)
@@ -68,8 +66,7 @@ def lazyframe_from_file_name_sheet(service, file_name:str, folder_id:str, **kwar
         while done is False:
             _status, done = downloader.next_chunk()
     except HttpError as error:
-        print(f'google drive error: {error}')
-        return
+        sys.exit(f'google drive error: {error}')
 
     file.seek(0) # after writing, pointer is at the end of the stream
     return pl.read_csv(file, **kwargs).lazy()
@@ -79,8 +76,7 @@ def lazyframe_from_id_and_sheetname(service, file_id:str, sheet_name:str, **kwar
     try:
         request = service.files().export_media(fileId=file_id, mimeType='application/vnd.openxmlformats-officedocument.spreadsheetml.sheet')
     except HttpError as error:
-        print(f'error checking google drive: {error}')
-        return
+        sys.exit(f'error checking google drive: {error}')
     file = io.BytesIO()
     downloader = MediaIoBaseDownload(file, request)
 
@@ -122,7 +118,7 @@ def awarxe(service, day:str='') -> pl.LazyFrame | None:
             print('folder not found')
             folder_id = None
     except HttpError as error:
-        print(f'error checking google drive: {error}')
+        sys.exit(f'error checking google drive: {error}')
 
     try:
         results = service.files().list(q=f"name = '{file_name}' and '{folder_id}' in parents",
@@ -134,11 +130,9 @@ def awarxe(service, day:str='') -> pl.LazyFrame | None:
             try:
                 request = service.files().get_media(fileId=file_id)
             except HttpError as error:
-                print(f'error checking google drive: {error}')
-                return
+                sys.exit(f'error checking google drive: {error}')
         else:
-            print('no file found')
-            return
+            sys.exit('no file found')
 
 
         file = io.BytesIO()
@@ -149,8 +143,7 @@ def awarxe(service, day:str='') -> pl.LazyFrame | None:
         while done is False:
             _status, done = downloader.next_chunk()
     except HttpError as error:
-        print(f'google drive error: {error}')
-        return
+        sys.exit(f'google drive error: {error}')
 
     file.seek(0) # after writing, pointer is at the end of the stream
     return pl.read_csv(file, separator='|', infer_schema_length=100000).lazy()
@@ -203,7 +196,7 @@ def upload_csv_as_sheet(service, file_name:str, folder_id:str) -> None:
         print (f'uploaded to: {file.get("webViewLink")}')
 
     except HttpError as error:
-        print(f'an error occurred: {error}')
+        sys.exit(f'an error occurred: {error}')
 
 
 def update_sheet(service, file_name:str, file_id:str) -> None:
@@ -225,43 +218,40 @@ def update_sheet(service, file_name:str, file_id:str) -> None:
         print (f'uploaded to: {file.get("webViewLink")}')
 
     except HttpError as error:
-        print(f'an error occurred: {error}')
+        sys.exit(f'an error occurred: {error}')
+
 
 def find_or_create_folder(service, folder_name:str, parent_folder_id:str) -> str:
     """
         goes into the google drive to find a folder with the pharmacy name, if it is not there it will create a new folder
-        function will also convert the top_pharmacy CSV into a google sheet and transfer it into the correct folder and provide a URL link for the folder
+        function will also convert the top_pharmacy csv into a google sheet and transfer it into the correct folder and provide a url link for the folder
     args:
         service: an authorized google drive service
         folder_name: the name of the new folder
         parent_folder_id: the id of the folder where the new folder should go
     returns:
-        folder ID: a string that will contain the ID of the folder created
+        folder_id: a string that will contain the id of the folder created
     """
-    folder_exists = False
     folder_id = None
     try:
         print(f'searching for folder: {folder_name}...')
         results = service.files().list(q=f"name = '{folder_name}' and '{parent_folder_id}' in parents", supportsAllDrives=True, includeItemsFromAllDrives=True).execute()
         files = results.get('files', [])
         if files:
-            folder_exists = True
             folder_id = files[0]['id']
+            folder_url = f"https://drive.google.com/drive/folders/{folder_id}"
+            print(f'folder exists at {folder_url}')
+            return folder_id
+        else:
+            file_metadata = {
+                'name': folder_name,
+                'parents': [parent_folder_id],
+                'mimeType': 'application/vnd.google-apps.folder',
+            }
+            folder = service.files().create(supportsAllDrives=True, body=file_metadata).execute()
+            folder_url = f"https://drive.google.com/drive/folders/{folder['id']}"
+            print(f'folder created at {folder_url}')
+            return folder['id']
+
     except HttpError as error:
-        print(f'error checking google drive: {error}')
-        return ''
-    if not folder_exists:
-        # if the folder doesn't exist, create it
-        file_metadata = {
-            'name': folder_name,
-            'parents': [parent_folder_id],
-            'mimeType': 'application/vnd.google-apps.folder',
-        }
-        folder = service.files().create(supportsAllDrives=True, body=file_metadata).execute()
-        folder_url = f"https://drive.google.com/drive/folders/{folder['id']}"
-        print(f'folder created at {folder_url}')
-        return folder['id']
-    else:
-        folder_url = f"https://drive.google.com/drive/folders/{folder_id}"
-        print(f'folder exists at {folder_url}')
-        return folder_id
+        sys.exit(f'error checking google drive: {error}')
