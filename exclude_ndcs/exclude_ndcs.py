9be613bff4291f28a2e0011a8f2d28dbea19a1d8
jake greenhalgh <1jake.g@gmail.com>
2024-11-19 15:57:40 -0700

    convert to using google drive instead of csv\

--- exclude_ndcs/exclude_ndcs.py
+++ exclude_ndcs/exclude_ndcs.py
@@ -1,7 +1,18 @@
 import polars as pl
-from utils import tableau
+from utils import tableau, auth, drive
 
-excluded_ndcs = pl.scan_csv('data/excluded_ndcs.csv', infer_schema_length=0) # infer_schema_length=0 forces all types to utf-8 and maintains the leading 0s in NDCs
+import toml
+from googleapiclient.discovery import build
+
+creds = auth.auth()
+service = build('drive', 'v3', credentials=creds)
+
+with open('../secrets.toml', 'r') as f:
+    secrets = toml.load(f)
+
+sheet_id = secrets['file']['excluded_ndcs']
+
+excluded_ndcs = drive.lazyframe_from_id_and_sheetname(service, sheet_id, 'excluded', infer_schema_length=0)
 
 luid = tableau.find_view_luid('opiate_antagonists', 'opiate antagonists')
 antagonists = (
@@ -22,5 +33,19 @@ else:
     print(new_ndcs)
 
     new_file = pl.concat([excluded_ndcs.collect(), new_ndcs])
-    new_file.write_csv('data/excluded_ndcs.csv')
-    print('data/excluded_ndcs.csv updated')
+
+    range_name = 'excluded!A:B'
+    service = build('sheets', 'v4', credentials=creds)
+    service.spreadsheets().values().clear(spreadsheetId=sheet_id,range=range_name).execute()
+    data = [list(row) for row in new_file.rows()]
+    data.insert(0, ['final_id', 'appearance_date'])
+    body = {
+        'values': data
+    }
+    result = service.spreadsheets().values().update(
+        spreadsheetId=sheet_id,
+        range=range_name,
+        valueInputOption='RAW',
+        body=body
+    ).execute()
+    print(f"{result.get('updatedCells')} cells updated.")
