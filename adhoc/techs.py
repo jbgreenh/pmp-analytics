67be30852552eb95f2732c862b14f6a3ec27913d
jbgreenh <jbgreenh@asu.edu>
2024-11-27 08:24:40 -0700

    updated to include tech trainee exp date

--- adhoc/techs.py
+++ adhoc/techs.py
@@ -20,17 +20,23 @@ superseded = (
 s_to_t = (
     techs.join(superseded, on='SSN', how='inner', suffix='_sup')
     .with_columns(
-        pl.col('Issue Date').sub(pl.col('Issue Date_sup')).alias('time_delta')
+        pl.col('Issue Date').sub(pl.col('Issue Date_sup')).alias('time_delta'),
+        pl.col('Issue Date').sub(pl.col('Expiration Date_sup')).alias('time_delta2'),
     )
     .with_columns(
-        pl.col('time_delta').dt.total_days().alias('days_to_tech')
+        pl.col('time_delta').dt.total_days().alias('days_to_tech'),
+        pl.col('time_delta2').dt.total_days().alias('days_to_tech_from_exp'),
     )
     .select(
         'License #', 'Type', 'Type_sup', 'Status', 'Status_sup',
         'First Name', 'Middle Name', 'Last Name', 'Issue Date',
-        'Issue Date_sup', 'days_to_tech'
+        'Issue Date_sup', 'Expiration Date_sup', 'days_to_tech', 'days_to_tech_from_exp'
     )
 )
 
 print(s_to_t.select('days_to_tech').describe())
-s_to_t.write_csv('data/techs/s_to_t.csv')
+print(s_to_t.select('days_to_tech_from_exp').describe())
+
+fp = 'data/techs/s_to_t.csv'
+s_to_t.write_csv(fp)
+print(f'{fp} written')
