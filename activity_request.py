f29d2cfa2c7af49da77ea5a765ad70234aecfbb1
jake g <1jake.g@gmail.com>
2025-04-01 14:59:58 -0700

    change select for dispensers

--- activity_request.py
+++ activity_request.py
@@ -41,52 +41,99 @@ def activity_request(request_type:str):
                 filters = {
                     'dea':dea, 'start_date':start_date, 'end_date':end_date
                 }
-                lf = (
-                    tableau.lazyframe_from_view_id(luid, filters=filters, infer_schema=False)
-                    .select(
-                        pl.col('Prescriber DEA'),
-                        pl.col('Prescriber NPI'),
-                        pl.col('Prescriber First Name'),
-                        pl.col('Prescriber Last Name'),
-                        pl.col('Orig Prescriber Address Line One').alias('Prescriber Address'),
-                        pl.col('Orig Prescriber Address Line Two').alias('Prescriber Address 2'),
-                        pl.col('Orig Prescriber City').alias('Prescriber City'),
-                        pl.col('Orig Prescriber State Abbr').alias('Prescriber State'),
-                        pl.col('Orig Prescriber Zip').alias('Prescriber ZIP'),
-                        pl.col('Pharmacy DEA'),
-                        pl.col('Pharmacy NPI'),
-                        pl.col('Pharmacy Retail Name').alias('Pharmacy Name'),
-                        pl.col('Orig Pharmacy Address Line One').alias('Pharmacy Address'),
-                        pl.col('Orig Pharmacy Address Line Two').alias('Pharmacy Address 2'),
-                        pl.col('Orig Pharmacy City').alias('Pharmacy City'),
-                        pl.col('Orig Pharmacy State Abbr').alias('Pharmacy State'),
-                        pl.col('Orig Pharmacy Zip').alias('Pharmacy ZIP'),
-                        pl.col('Pharmacy Chain Site Id'),
-                        pl.col('Prescription Number').alias('Rx Number'),
-                        pl.col('Day of Filled At').alias('Rx Fill Date'),
-                        pl.col('Day of Written At').alias('Rx Written Date'),
-                        pl.col('Refills Authorized').alias('Authorized Refills'),
-                        pl.col('Refill Y/N'),
-                        pl.col('Payment Type'),
-                        pl.col('Drug Schedule'),
-                        pl.col('AHFS Description').alias('AHFS Drug Category'),
-                        pl.col('Brand Name').alias('Drug Name'),
-                        pl.col('Quantity'),
-                        pl.col('Strength'),
-                        pl.col('Dosage Type'),
-                        pl.col('Days Supply'),
-                        pl.col('Daily MME'),
-                        pl.col('Patient First Name'),
-                        pl.col('Patient Last Name'),
-                        pl.col('Day of Patient Birthdate').alias('Patient DOB'),
-                        pl.col('Orig Patient Address Line One').alias('Patient Address'),
-                        pl.col('Orig Patient Address Line Two').alias('Patient Address 2'),
-                        pl.col('Orig Patient City').alias('Patient City'),
-                        pl.col('Orig Patient State Abbr').alias('Patient State'),
-                        pl.col('Orig Patient Zip').alias('Patient ZIP'),
-                        pl.col('Veterinarian Prescription Y/N')
+                if request_type == 'prescriber':
+                    lf = (
+                        tableau.lazyframe_from_view_id(luid, filters=filters, infer_schema=False)
+                        .select(
+                            pl.col('Prescriber DEA'),
+                            pl.col('Prescriber NPI'),
+                            pl.col('Prescriber First Name'),
+                            pl.col('Prescriber Last Name'),
+                            pl.col('Orig Prescriber Address Line One').alias('Prescriber Address'),
+                            pl.col('Orig Prescriber Address Line Two').alias('Prescriber Address 2'),
+                            pl.col('Orig Prescriber City').alias('Prescriber City'),
+                            pl.col('Orig Prescriber State Abbr').alias('Prescriber State'),
+                            pl.col('Orig Prescriber Zip').alias('Prescriber ZIP'),
+                            pl.col('Pharmacy DEA'),
+                            pl.col('Pharmacy NPI'),
+                            pl.col('Pharmacy Retail Name').alias('Pharmacy Name'),
+                            pl.col('Orig Pharmacy Address Line One').alias('Pharmacy Address'),
+                            pl.col('Orig Pharmacy Address Line Two').alias('Pharmacy Address 2'),
+                            pl.col('Orig Pharmacy City').alias('Pharmacy City'),
+                            pl.col('Orig Pharmacy State Abbr').alias('Pharmacy State'),
+                            pl.col('Orig Pharmacy Zip').alias('Pharmacy ZIP'),
+                            pl.col('Pharmacy Chain Site Id'),
+                            pl.col('Prescription Number').alias('Rx Number'),
+                            pl.col('Day of Filled At').alias('Rx Fill Date'),
+                            pl.col('Day of Written At').alias('Rx Written Date'),
+                            pl.col('Refills Authorized').alias('Authorized Refills'),
+                            pl.col('Refill Y/N'),
+                            pl.col('Payment Type'),
+                            pl.col('Drug Schedule'),
+                            pl.col('AHFS Description').alias('AHFS Drug Category'),
+                            pl.col('Brand Name').alias('Drug Name'),
+                            pl.col('Quantity'),
+                            pl.col('Strength'),
+                            pl.col('Dosage Type'),
+                            pl.col('Days Supply'),
+                            pl.col('Daily MME'),
+                            pl.col('Patient First Name'),
+                            pl.col('Patient Last Name'),
+                            pl.col('Day of Patient Birthdate').alias('Patient DOB'),
+                            pl.col('Orig Patient Address Line One').alias('Patient Address'),
+                            pl.col('Orig Patient Address Line Two').alias('Patient Address 2'),
+                            pl.col('Orig Patient City').alias('Patient City'),
+                            pl.col('Orig Patient State Abbr').alias('Patient State'),
+                            pl.col('Orig Patient Zip').alias('Patient ZIP'),
+                            pl.col('Veterinarian Prescription Y/N')
+                        )
                     )
-                )
+                else:   # dispenser
+                    lf = (
+                        tableau.lazyframe_from_view_id(luid, filters=filters, infer_schema=False)
+                        .select(
+                            pl.col('Pharmacy DEA'),
+                            pl.col('Pharmacy NPI'),
+                            pl.col('Pharmacy Retail Name').alias('Pharmacy Name'),
+                            pl.col('Orig Pharmacy Address Line One').alias('Pharmacy Address'),
+                            pl.col('Orig Pharmacy Address Line Two').alias('Pharmacy Address 2'),
+                            pl.col('Orig Pharmacy City').alias('Pharmacy City'),
+                            pl.col('Orig Pharmacy State Abbr').alias('Pharmacy State'),
+                            pl.col('Orig Pharmacy Zip').alias('Pharmacy ZIP Code'),
+                            pl.col('Pharmacy Chain Site Id'),
+                            pl.col('Patient First Name'),
+                            pl.col('Patient Last Name'),
+                            pl.col('Day of Patient Birthdate').alias('Patient DOB'),
+                            pl.col('Orig Patient Address Line One').alias('Patient Address'),
+                            pl.col('Orig Patient Address Line Two').alias('Patient Address 2'),
+                            pl.col('Orig Patient City').alias('Patient City'),
+                            pl.col('Orig Patient State Abbr').alias('Patient State'),
+                            pl.col('Orig Patient Zip').alias('Patient ZIP Code'),
+                            pl.col('Prescription Number').alias('Rx Number'),
+                            pl.col('Day of Filled At').alias('Rx Fill Date'),
+                            pl.col('Day of Written At').alias('Rx Written Date'),
+                            pl.col('Refills Authorized').alias('Authorized Refills'),
+                            pl.col('Refill Y/N'),
+                            pl.col('Payment Type'),
+                            pl.col('Drug Schedule'),
+                            pl.col('AHFS Description').alias('AHFS Drug Category'),
+                            pl.col('Brand Name').alias('Drug Name'),
+                            pl.col('Quantity'),
+                            pl.col('Strength'),
+                            pl.col('Dosage Type'),
+                            pl.col('Days Supply'),
+                            pl.col('Daily MME'),
+                            pl.col('Prescriber DEA'),
+                            pl.col('Prescriber NPI'),
+                            pl.col('Prescriber First Name'),
+                            pl.col('Prescriber Last Name'),
+                            pl.col('Orig Prescriber Address Line One').alias('Prescriber Address'),
+                            pl.col('Orig Prescriber Address Line Two').alias('Prescriber Address 2'),
+                            pl.col('Orig Prescriber City').alias('Prescriber City'),
+                            pl.col('Orig Prescriber State Abbr').alias('Prescriber State'),
+                            pl.col('Orig Prescriber Zip').alias('Prescriber ZIP Code'),
+                            pl.col('Veterinarian Prescription Y/N'))
+                        )
 
                 file_name = f'{dea}_{start_date}_-_{end_date}'
                 file_path = f'data/{request_type}_activity_request/{file_name}.csv'
@@ -102,7 +149,7 @@ def activity_request(request_type:str):
                     df.write_csv(file_path)
                     print(f'{file_path} written')
     else:
-        sys.exit('no pdfs in data folder')
+        sys.exit(f'no pdfs in data/{request_type}/ folder')
 
 def main():
     parser = argparse.ArgumentParser(description='pull a request')
