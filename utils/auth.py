8e44b4eabb3508fdcd053b0a0a1b931f56f0c919
jakeg <1jake.g@gmail.com>
2025-08-28 15:29:02 -0700

    refactor

--- utils/auth.py
+++ utils/auth.py
@@ -1,12 +1,18 @@
-
 from pathlib import Path
 
+import google.auth.external_account_authorized_user
+import google.oauth2.credentials
 from google.auth.transport.requests import Request
-from google.oauth2.credentials import Credentials
 from google_auth_oauthlib.flow import InstalledAppFlow
 
 
-def auth():
+def auth() -> google.oauth2.credentials.Credentials | google.auth.external_account_authorized_user.Credentials:
+    """
+    authorization for google services
+
+    returns:
+        creds: credentials for building google services
+    """
     # If modifying these scopes, delete the file token.json. scopes here: https://developers.google.com/identity/protocols/oauth2/scopes#drive
     scopes = ['https://www.googleapis.com/auth/drive', 'https://www.googleapis.com/auth/spreadsheets',
               'https://www.googleapis.com/auth/documents', 'https://www.googleapis.com/auth/gmail.send']
@@ -16,7 +22,7 @@ def auth():
     # created automatically when the authorization flow completes for the first
     # time.
     if Path('token.json').exists():
-        creds = Credentials.from_authorized_user_file('token.json', scopes)
+        creds = google.oauth2.credentials.Credentials.from_authorized_user_file('token.json', scopes)
     # If there are no (valid) credentials available, let the user log in.
     if not creds or not creds.valid:
         if creds and creds.expired and creds.refresh_token:
@@ -26,5 +32,5 @@ def auth():
                 'credentials.json', scopes)
             creds = flow.run_local_server(port=0)
         # Save the credentials for the next run
-        Path('token.json').write_text(creds.to_json())
+        Path('token.json').write_text(creds.to_json(), encoding='utf-8')
     return creds
