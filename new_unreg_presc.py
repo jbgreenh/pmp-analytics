3240556d7cbcc8499892cc6ba7c76dfc48756aef
jake g <1jake.g@gmail.com>
2025-09-16 11:29:06 -0700

    add arg to run test without sending emails

--- new_unreg_presc.py
+++ new_unreg_presc.py
@@ -1,3 +1,4 @@
+import argparse
 import os
 import sys
 from dataclasses import dataclass, field
@@ -69,11 +70,11 @@ def get_board_contacts(service) -> dict:    # noqa: ANN001 | service is dynamica
     boards_dict = {}
     for board in boards:
         bn = board_contacts.filter(pl.col('Board') == board)['Board Name'].first()
-        if bn is not str:
+        if not isinstance(bn, str):
             sys.exit(f'board name not found for {board}')
         be = board_contacts.filter(pl.col('Board') == board)['Email'].first()
-        if be is not str:
-            sys.exit(f'board email not found {board}')
+        if not isinstance(be, str):
+            sys.exit(f'board email not found for {board}')
         boards_dict[board] = BoardInfo(board_name=bn, board_emails=be)
     return boards_dict
 
@@ -309,7 +310,7 @@ def add_dfs_to_board_info(service, unreg_presc: pl.LazyFrame, board_info: dict) 
     return board_info
 
 
-def send_emails(board_dict: dict[str, BoardInfo], creds: google.oauth2.credentials.Credentials | google.auth.external_account_authorized_user.Credentials, drive_service) -> None:    # noqa: ANN001 | service is dynamically typed
+def send_emails(board_dict: dict[str, BoardInfo], creds: google.oauth2.credentials.Credentials | google.auth.external_account_authorized_user.Credentials, drive_service, *, send: bool = True) -> None:    # noqa: ANN001 | service is dynamically typed
     """
     sends emails to each board with their unregistered prescribers
 
@@ -317,6 +318,7 @@ def send_emails(board_dict: dict[str, BoardInfo], creds: google.oauth2.credentia
         board_dict: the `board_dict` returned by `add_dfs_to_board_info()`
         creds: google api credentials
         drive_service: a google drive service
+        send: a boolean indicating whether to send the emails (True) or to print info on the emails that would be sent (False)
     """
     def remove_first_page(export: BytesIO, file_path: Path) -> None:
         """this removes the broken header first page google drive exports create for some reason"""
@@ -376,11 +378,23 @@ def send_emails(board_dict: dict[str, BoardInfo], creds: google.oauth2.credentia
             file_paths=[report_file, rrn_path, flyer_path],
             bcc=os.environ['EMAIL_COMPLIANCE']
         )
-        email.send_email(message)
+        if send:
+            email.send_email(message)
+        else:
+            print(f'email to {board} board')
+            print(f'from: {message.sender}')
+            print(f'to: {message.to}')
+            print(f'subj: {message.subject}')
+            print(message.file_paths)
+            print(info.board_df.head())
         Path(report_file).unlink()
 
 
 if __name__ == '__main__':
+    parser = argparse.ArgumentParser(description='check unregistered prescribers')
+    parser.add_argument('-ne', '--no-email', action='store_false', help='do not send emails')
+    args = parser.parse_args()
+
     load_dotenv()
     creds = auth.auth()
     service = build('drive', 'v3', credentials=creds)
@@ -394,3 +408,10 @@ if __name__ == '__main__':
     board_info = update_board_info_with_uploaders(board_contacts)
 
     full_board_info = add_dfs_to_board_info(service, unregistered_w_boards, board_info)
+
+    send_emails(full_board_info, creds, service, send=args.no_email)
+
+    board_counts = unregistered_w_boards.collect()['board'].value_counts(sort=True)
+    print('board unregistered counts (written to clipboard):')
+    print(board_counts)
+    board_counts.write_clipboard()
