8744d2520c84fb32f78449830df448ddbabc8475
jakeg <1jake.g@gmail.com>
2025-09-18 14:43:39 -0700

    update comment

--- utils/constants.py
+++ utils/constants.py
@@ -4,4 +4,4 @@ DAYS_DELINQUENT_THRESHOLD = 7               # min days delinquent that will put 
 JULY_MONTH_NUMBER = 7
 MAX_SERVU_FILE_COUNT = 5                    # the max number of files to keep on the servu
 PHX_TZ = ZoneInfo('America/Phoenix')        # phoenix timezone
-TOP_PRESCRIBERS = 20                        # number of prescribers with the most dispensations and no searches
+TOP_PRESCRIBERS = 20                        # number of prescribers with the most dispensations and no searches for mandatory use reporting
