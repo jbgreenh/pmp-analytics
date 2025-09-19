38b90960ecb2335cb1ef46a59f3596cc130b5922
jakeg <1jake.g@gmail.com>
2025-09-19 10:53:56 -0700

    test for specific field properties

--- tests/test_utils/test_deas.py
+++ tests/test_utils/test_deas.py
@@ -1,3 +1,5 @@
+import re
+
 import polars as pl
 
 from utils import deas
@@ -5,8 +7,21 @@ from utils import deas
 
 def test_deas() -> None:
     """tests that deas util leads to a non-empty lazyframe for all valid p values"""
-    p_list = ['all', 'az', 'pharm', 'presc']
-    for p in p_list:
-        deas_lf = deas.deas(p)  # type: ignore[reportArgumentType]
-        assert isinstance(deas_lf, pl.LazyFrame)
-        assert not deas_lf.collect().is_empty()
+    deas_lf = deas.deas('all', az=False)
+    assert isinstance(deas_lf, pl.LazyFrame)
+
+    deas_df = deas_lf.collect()
+
+    pattern = r'^[ABCFGHMPRabcfghmpr][A-Za-z](?:[0-9]{6}[1-9]|[0-9]{5}[1-9][0-9]|[0-9]{4}[1-9][0-9]{2}|[0-9]{3}[1-9][0-9]{3}|[0-9]{2}[1-9][0-9]{4}|[0-9][1-9][0-9]{5}|[1-9][0-9]{6})$'
+    alt_pat = r'^[AFPRBMafprbm](?:[0-9]{8})'
+    for dea in set(deas_df['DEA Number']):
+        assert re.match(pattern, dea) or re.match(alt_pat, dea)
+
+    for ac in set(deas_df['Business Activity Code']):
+        assert ac.isalpha()
+
+    for zip_code in set(deas_df['Zip Code']):
+        assert zip_code.isnumeric()
+
+    for ssn in set(deas_df['SSN']):
+        assert ssn.isnumeric() or not ssn
