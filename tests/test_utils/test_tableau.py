9d96248d3c42fdd1ee040cbba01374577a59fc28
jake g <1jake.g@gmail.com>
2025-09-23 14:56:28 -0700

    test empty view

--- tests/test_utils/test_tableau.py
+++ tests/test_utils/test_tableau.py
@@ -12,7 +12,7 @@ load_dotenv()
 def test_find_view_luid() -> None:
     """test find_view_luid function"""
     luid = tableau.find_view_luid(view_name='testdisp', workbook_name='testing')
-    assert luid == os.environ['TEST_LUID']
+    assert luid == os.environ['TESTDISP_LUID']
 
     with pytest.raises(tableau.TableauLUIDNotFoundError):
         _luid = tableau.find_view_luid(view_name='fake view', workbook_name='testing')
@@ -20,8 +20,11 @@ def test_find_view_luid() -> None:
 
 def test_lazyframe_from_view_id() -> None:
     """test lazyframe_from_view_id"""
-    lf = tableau.lazyframe_from_view_id(os.environ['TEST_LUID'])
+    lf = tableau.lazyframe_from_view_id(os.environ['TESTDISP_LUID'])
     assert isinstance(lf, pl.LazyFrame)
     df = lf.collect()
     assert df.columns == ['test_var']
     assert df['test_var'].first() == 64_209
+
+    with pytest.raises(tableau.TableauNoDataError):
+        _lf = tableau.lazyframe_from_view_id(os.environ['TESTEMPTY_LUID'])
