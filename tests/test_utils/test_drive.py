ef7412a98651d03f74e0f92d7b0219346fb22f0d
jake g <1jake.g@gmail.com>
2025-09-23 10:26:42 -0700

    add test_lazyframe_from_file_name and test_get_latest_upload

--- tests/test_utils/test_drive.py
+++ tests/test_utils/test_drive.py
@@ -1,6 +1,8 @@
-from datetime import date
+import os
+from datetime import date, datetime
 
 import polars as pl
+from dotenv import load_dotenv
 from googleapiclient.discovery import build
 
 from utils import auth, drive
@@ -26,6 +28,7 @@ AWARXE_SCHEMA = pl.Schema({
     'registration review date': str
 })
 
+load_dotenv()
 creds = auth.auth()
 service = build('drive', 'v3', credentials=creds)
 
@@ -41,3 +44,47 @@ def test_awarxe() -> None:
     awarxe = drive.awarxe(service)
     assert isinstance(awarxe, pl.LazyFrame)
     assert awarxe.collect_schema() == AWARXE_SCHEMA
+
+
+def test_lazyframe_from_filename() -> None:
+    """test the lazyframe_from_filename function"""
+    test_folder = os.environ['TEST_FOLDER']
+    test_sheet = drive.lazyframe_from_file_name(service, 'test sheet', folder_id=test_folder, drive_ft='sheet')
+    assert isinstance(test_sheet, pl.LazyFrame)
+    test_sheet_df = test_sheet.collect()
+    assert test_sheet_df['a'].first() == 1
+    assert test_sheet_df['a'].sum() == 6
+    assert test_sheet_df['a'].last() == 2
+    assert test_sheet_df['b'].first() == 2
+    assert test_sheet_df['b'].last() == 3
+    assert test_sheet_df['b'].sum() == 7
+    assert test_sheet_df['c'].first() == 3
+    assert test_sheet_df['c'].last() == 1
+    assert test_sheet_df['c'].sum() == 5
+
+    test_csv = drive.lazyframe_from_file_name(service, 'test_csv.csv', folder_id=test_folder, drive_ft='csv')
+    assert isinstance(test_csv, pl.LazyFrame)
+    test_csv_df = test_csv.collect()
+    assert test_csv_df['a'].first() == 1
+    assert test_csv_df['a'].sum() == 6
+    assert test_csv_df['a'].last() == 2
+    assert test_csv_df['b'].first() == 2
+    assert test_csv_df['b'].last() == 3
+    assert test_csv_df['b'].sum() == 7
+    assert test_csv_df['c'].first() == 3
+    assert test_csv_df['c'].last() == 1
+    assert test_csv_df['c'].sum() == 5
+
+
+def test_get_latest_upload() -> None:
+    """test the get_latest_upload function"""
+    latest_file = drive.get_latest_uploaded(service, folder_id=os.environ['TEST_UPLOADS_FOLDER'], drive_ft='sheet')
+    assert isinstance(latest_file.created_at, datetime)
+    assert latest_file.created_at.month == 9
+    assert latest_file.created_at.day == 23
+    assert latest_file.created_at.year == 2025
+
+    assert isinstance(latest_file.lf, pl.LazyFrame)
+    latest_df = latest_file.lf.collect()
+    assert latest_df.columns == ['new']
+    assert latest_df['new'].first() == 2
