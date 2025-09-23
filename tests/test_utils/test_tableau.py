5f74f3321888d5664f5b7a38765f56ccf605cdd3
jake g <1jake.g@gmail.com>
2025-09-23 14:32:19 -0700

    add tableau util testing

--- tests/test_utils/test_tableau.py
+++ tests/test_utils/test_tableau.py
@@ -1,1 +1,28 @@
-# TODO: create test workbook on tableau that does not change for testing
+import os
+
+import polars as pl
+import pytest
+from dotenv import load_dotenv
+
+from utils import tableau
+
+load_dotenv()
+
+
+def test_find_view_luid() -> None:
+    """test find_view_luid function"""
+    luid = tableau.find_view_luid(view_name='testdisp', workbook_name='testing')
+    assert luid == os.environ['TEST_LUID']
+
+    with pytest.raises(tableau.TableauLUIDNotFoundError):
+        _luid = tableau.find_view_luid(view_name='fake view', workbook_name='testing')
+
+
+def test_lazyframe_from_view_id() -> None:
+    """test lazyframe_from_view_id"""
+    luid = tableau.find_view_luid(view_name='testdisp', workbook_name='testing')
+    lf = tableau.lazyframe_from_view_id(luid)
+    assert isinstance(lf, pl.LazyFrame)
+    df = lf.collect()
+    assert df.columns == ['test_var']
+    assert df['test_var'].first() == 64_209
