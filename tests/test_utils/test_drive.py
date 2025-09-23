b2639460038c5cb0eb8477a180ad7605da7c5eef
jake g <1jake.g@gmail.com>
2025-09-23 13:18:03 -0700

    add test_update_sheet

--- tests/test_utils/test_drive.py
+++ tests/test_utils/test_drive.py
@@ -1,5 +1,6 @@
 import os
 from datetime import date, datetime
+from pathlib import Path
 
 import polars as pl
 import pytest
@@ -105,3 +106,71 @@ def test_folder_id_from_name() -> None:
     _response = service.files().delete(fileId=make_folder, supportsAllDrives=True).execute()
     with pytest.raises(drive.GoogleDriveNotFoundError):
         _fake_folder = drive.folder_id_from_name(service, folder_name='make folder', parent_folder_id=os.environ['TEST_FOLDER'])
+
+
+def test_upload_csv_as_sheet() -> None:
+    """test upload_csv_as_sheet function"""
+    test_df = pl.DataFrame({
+        'a': [1, 1, 1, 6],
+        'b': [2, 2, 2, 9],
+    })
+    file_path = Path('data/test_df_upload.csv')
+    test_df.write_csv(file_path)
+    test_folder = os.environ['TEST_FOLDER']
+    drive.upload_csv_as_sheet(service, file_path=file_path, folder_id=test_folder)
+    file_path.unlink()
+    upload_csv = drive.lazyframe_from_file_name(service, file_name='test_df_upload', folder_id=test_folder, drive_ft='sheet').collect()
+
+    assert test_df.schema == upload_csv.schema
+    assert upload_csv['a'].sum() == 9
+    assert upload_csv['a'].first() == 1
+    assert upload_csv['a'].last() == 6
+    assert upload_csv['b'].sum() == 15
+    assert upload_csv['b'].first() == 2
+    assert upload_csv['b'].last() == 9
+
+    results = service.files().list(q=f"name = 'test_df_upload' and '{test_folder}' in parents", supportsAllDrives=True, includeItemsFromAllDrives=True).execute()
+    files = results.get('files', [])
+    _response = service.files().delete(fileId=files[0]['id'], supportsAllDrives=True).execute()
+
+
+def test_update_sheet() -> None:
+    """test update_sheet function"""
+    test_df = pl.DataFrame({
+        'd': [1, 2, 3],
+        'e': [3, 2, 1],
+        'f': [2, 3, 1],
+    })
+    fp = Path('data/test_update.csv')
+    test_df.write_csv(fp)
+    drive.update_sheet(service, fp, file_id=os.environ['TEST_SHEET'])
+    test_sheet = drive.lazyframe_from_file_name(service, file_name='test sheet', folder_id=os.environ['TEST_FOLDER'], drive_ft='sheet').collect()
+    assert test_sheet['d'].first() == 1
+    assert test_sheet['d'].last() == 3
+    assert test_sheet['d'].sum() == 6
+    assert test_sheet['e'].first() == 3
+    assert test_sheet['e'].last() == 1
+    assert test_sheet['e'].sum() == 6
+    assert test_sheet['f'].first() == 2
+    assert test_sheet['f'].last() == 1
+    assert test_sheet['f'].sum() == 6
+
+    # restore to original
+    test_df = pl.DataFrame({
+        'a': [1, 3, 2],
+        'b': [2, 2, 3],
+        'c': [3, 1, 1],
+    })
+    test_df.write_csv(fp)
+    drive.update_sheet(service, fp, file_id=os.environ['TEST_SHEET'])
+    test_sheet = drive.lazyframe_from_file_name(service, file_name='test sheet', folder_id=os.environ['TEST_FOLDER'], drive_ft='sheet').collect()
+    assert test_sheet['a'].first() == 1
+    assert test_sheet['a'].sum() == 6
+    assert test_sheet['a'].last() == 2
+    assert test_sheet['b'].first() == 2
+    assert test_sheet['b'].last() == 3
+    assert test_sheet['b'].sum() == 7
+    assert test_sheet['c'].first() == 3
+    assert test_sheet['c'].last() == 1
+    assert test_sheet['c'].sum() == 5
+    fp.unlink()
