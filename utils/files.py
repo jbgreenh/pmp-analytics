6fecd8e234ddef4c01b51a922682046a3f108a6f
jake g <1jake.g@gmail.com>
2025-10-28 11:23:12 -0700

    max_age_days -> max_age_hours

--- utils/files.py
+++ utils/files.py
@@ -6,16 +6,16 @@ from pathlib import Path
 from utils.constants import PHX_TZ
 
 
-def warn_file_age(file: Path, max_age_days: int = 1) -> None:
+def warn_file_age(file: Path, max_age_hours: int = 12) -> None:
     """
-    warns if file age of input file is older than 24 hours and prompts the user for if they want to continue with the script
+    warns if file age of input file is older than 12 hours and prompts the user for if they want to continue with the script
 
     args:
         file: Path to the file in question
-        max_age_days: the max age in days the file can be before triggering this warning
+        max_age_hours: the max age, in hours, the file can be before triggering this warning
     """
     file_age = datetime.now(tz=PHX_TZ) - datetime.fromtimestamp(file.stat().st_mtime, tz=PHX_TZ)
-    if file_age.days > max_age_days:
+    if (file_age.total_seconds() / 60 / 60) > max_age_hours:
         msg = (
             f'`{file}` has not been updated recently!\n'
             f'the file is {file_age.days} days and {round(file_age.seconds / 60 / 60, 2)} hours old\n'
