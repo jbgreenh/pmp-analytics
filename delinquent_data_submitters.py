5b1094c41ea6925b68c9b1428acd5516bc34c598
jakeg <1jake.g@gmail.com>
2025-12-12 09:56:00 -0700

    replace pharm_clean with delinquent_data_submitters

--- delinquent_data_submitters.py
+++ delinquent_data_submitters.py
@@ -1,71 +1,18 @@
-# daily notices (along with list of closed pharmacies in manage pharmacies)
-# special notices on fridays
-# hard deadline 30 business days after first friday notice
-# email logs with sent_dt, to, permit_number, days_delinquent, zip, email_type
-# other sheet will have active 7+ days delinquent pharmacies
-# last sheet will have pharmacies who have had complaints opened
+import argparse
 import os
 from calendar import FRIDAY, WEDNESDAY
 from datetime import datetime, timedelta
 from pathlib import Path
-from time import sleep
 from typing import Literal
 
 import polars as pl
 from dotenv import load_dotenv
+from googleapiclient.discovery import build
 
-from utils import drive, files, num_and_dt
+from utils import auth, drive, email, files, num_and_dt
 from utils.constants import DAYS_DELINQUENT_THRESHOLD, PHX_TZ
 
 type EmailType = Literal['daily', 'friday']
-
-
-def find_closed(mp_path: Path, lr_path: Path) -> pl.LazyFrame:
-    """
-    finds pharmacies from manage pharmacies that are not open in igov
-
-    args:
-        mp_path: path to the manage pharmacies file
-        lr_path: path to the list request file
-
-    returns:
-        list of pharmacies from manage pharmacies that are not open in igov as a lazyframe
-    """
-    mp = (
-        pl.scan_csv(mp_path)
-        .with_columns(
-            pl.col('DEA').str.strip_chars().str.to_uppercase()
-        )
-        .filter(
-            pl.col('Reporting Requirements') != 'Exempt'
-        )
-        .select('DEA', 'Pharmacy License Number', 'Reporting Requirements')
-    )
-
-    igov = (
-        pl.scan_csv(lr_path, infer_schema=False)
-        .filter(
-            pl.col('Type') == 'Pharmacy'
-        )
-        .with_columns(
-            pl.col('License/Permit #').str.strip_chars().str.to_uppercase()
-        )
-        .rename(
-            {'License/Permit #': 'Pharmacy License Number'}
-        )
-        .select(
-            'Pharmacy License Number', 'Status', 'Business Name', 'Street Address', 'Apt/Suite #',
-            'City', 'State', 'Zip', 'Email', 'Phone'
-        )
-    )
-
-    return (
-        mp
-        .join(igov, on='Pharmacy License Number', how='left', coalesce=True)
-        .filter(
-            pl.col('Status').str.starts_with('OPEN').not_()
-        )
-    )
 
 
 def process_input_files(mp_path: Path, dds_path: Path, lr_path: Path) -> pl.LazyFrame:
@@ -144,9 +91,12 @@ def process_input_files(mp_path: Path, dds_path: Path, lr_path: Path) -> pl.Lazy
             'Primary User',
             pl.concat_list(pl.col('Primary Email').str.to_lowercase(), pl.col('mp_email'), pl.col('igov_email')).list.unique().list.join(',').alias('to'),
             pl.concat_list(pl.col('Phone').str.to_lowercase(), pl.col('mp_phone'), pl.col('igov_phone')).list.unique().list.join(',').alias('phone_numbers'),
+            'Street Address',
+            'Apt/Suite #',
+            'City',
+            'State',
             'Zip',
         )
-        # TODO: add schema to deadlines so join doesn't complain if deadlines file is empty. see: lf.match_to_schema)
         .sort(pl.col('Days Delinquent'), descending=True)
     )
 
@@ -173,22 +123,78 @@ def date_in_next_week(lf: pl.LazyFrame) -> pl.LazyFrame:
     )
 
 
-def send_notices(dds: pl.LazyFrame, email_type: EmailType) -> None:
-    if email_type == 'friday':
-        print('friday notices')
-        # TODO: set email subj, body, from, to, etc
-        notices = dds.collect()
-    else:
-        print('daily notices')
-        # TODO: set email subj, body, etc
-        notices = dds.collect()
+def send_notices(lf: pl.LazyFrame, email_type: EmailType) -> None:
+    """
+    send dds email notices (or create drafts)
 
+    args:
+        lf: a lazyframe with the dds recipients
+        email_type: daily or friday notices
+    """
+    creds = auth.auth()
+    service = build('gmail', 'v1', credentials=creds)
     timestamps = []
-    for _row in notices.iter_rows(named=True):
-        # TODO: send emails bcc: compliance
-        sleep(.25)
+    notices = lf.collect()
+    for row in notices.iter_rows(named=True):
+        pharmacy_address = f'{row['Street Address']}, {row['Apt/Suite #']}\n{row['City']}, {row['State']} {row['Zip']}' if row['Apt/Suite #'] else f'{row['Street Address']}\n{row['City']}, {row['State']} {row['Zip']}'
+        last_compliant = row['Last Compliant'] or 'data has never been received'
+
+        if email_type == 'friday':
+            subject = f'CSPMP Action Required: Possible Complaint Against {row['Pharmacy License Number']}'
+            body = f"""
+            ATTENTION: {row['Business Name']}
+            {pharmacy_address}
+            {row['Pharmacy License Number']}
+            {row['DEA']}
+
+            According to our records, your pharmacy is not submitting daily reports to the Arizona Controlled Substance Prescription Monitoring Program clearinghouse.
+
+            Last date of data submission received on: <b>{last_compliant}</b>
+
+            At this time, you are in violation of <a href="https://www.azleg.gov/ars/36/02608.htm" target="_blank">ARS § 36-2608</a> reporting requirements. <em><b>Failure to upload your delinquent schedule II-V dispensations will result in a complaint being opened against the pharmacy.</b></em>
+
+            <b>Zero reports should be submitted for any days there are no controlled substance dispensations. For days you are not operational, you should report zero for those days on your next open business day.</b>
+
+            <b style='color: red;'>Please ensure you upload your missed submissions by {row['deadline']} or a complaint will be opened against the pharmacy permit. </b>
+
+            If your pharmacy has an active DEA number, an active AZ pharmacy permit, and is not limited to veterinary dispensing, <em><b>it is required to submit a daily report, including zero reports, for controlled substances II-V.</b></em>
+
+            If your pharmacy utilizes a vendor to submit dispensations on your behalf, please contact your vendor immediately to get this issue corrected to avoid possible Board action. You may forward this email to the appropriate vendor contact for assistance.
+
+            If you are receiving this message and you are a data vendor reporting submissions on behalf of the pharmacy, please be aware that the pharmacy is delinquent in AZ CSPMP reporting and faces possible Board action if not corrected by the given deadline. Please forward this information to the appropriate members of the pharmacy team if necessary.
+
+            If you have any questions or concerns about the data submission process, please contact Bamboo Health for Technical Support directly at 1-855-929-4767. Technical Support is available 24 hours a day, 7 days a week.
+
+            <a href="https://drive.google.com/file/d/1R1wCymw9T5n2sqn8fQGuWeEoCChXmjB0/view?ts=67ca02cd" target="_blank">AZ Data Submission Dispenser Guide</a>
+            <a href="https://pharmacypmp.az.gov/data-submissions-faqs" target="_blank">AZ Data Submission FAQs</a>
+            """
+        else:
+            subject = f'Notice of Missing CSPMP Data Submissions for {row['Pharmacy License Number']}'
+            body = f"""
+            <b>At this time, your pharmacy, {row['Pharmacy License Number']}, is in violation of <a href="https://www.azleg.gov/ars/36/02608.htm" target="_blank">ARS § 36-2608</a> reporting requirements.</b>
+
+            You are receiving this email because you are listed as the party responsible for submitting controlled substance dispensing information for the above-referenced dispenser to the Arizona Controlled Substances Prescription Monitoring Program (AZ CSPMP). Controlled substance dispensing information has not been received since <b>{last_compliant}</b>.
+
+            Please upload your schedule II-V dispensations DAILY, including zero reports, to avoid being noncompliant, and make sure to upload any days that were missed.
+
+            *Businesses closed for the weekend will still need to report on the following business day (Ex. on Monday report zero reports for Saturday and Sunday).
+
+            If you have any questions or concerns about the data submission process, please contact Bamboo Health for Technical Support directly at 1-855-929-4767. Technical Support is available 24 hours a day, 7 days a week.
+
+            <a href="https://drive.google.com/file/d/1R1wCymw9T5n2sqn8fQGuWeEoCChXmjB0/view?ts=67ca02cd" target="_blank">AZ Data Submission Dispenser Guide</a>
+            <a href="https://pharmacypmp.az.gov/data-submissions-faqs" target="_blank">AZ Data Submission FAQs</a>
+            """
+        msg = email.EmailMessage(
+            sender=os.environ['EMAIL_COMPLIANCE'],
+            to=row['to'],
+            subject=subject,
+            message_text=body,
+            monospace=True,
+        )
+        email.send_email(msg, service=service, draft=(not args.send_emails))
         ts = datetime.now(tz=PHX_TZ)
         timestamps.append(ts)
+
     ts_series = pl.Series(name='sent_dt', values=timestamps, dtype=pl.Datetime)
     notices.insert_column(0, ts_series)
 
@@ -213,9 +219,18 @@ def send_notices(dds: pl.LazyFrame, email_type: EmailType) -> None:
 
 
 def pharm_clean(dds: pl.LazyFrame) -> None:
+    """
+    takes the proper action for the delinquent data submitters process based on the day of the week
+
+    args:
+        dds: the dds lazyframe returned by `process_input_files()`
+    """
     today = datetime.now(tz=PHX_TZ).date()
     if WEDNESDAY <= today.weekday() <= FRIDAY:  # remove pharmacies that are no longer delinquent from deadlines list
-        deadlines = drive.lazyframe_from_id_and_sheetname(os.environ['DDS_DEADLINES_FILE'], 'deadlines', infer_schema_length=0)  # read_excel does not have infer_schema
+        deadlines = (
+            drive.lazyframe_from_id_and_sheetname(os.environ['DDS_DEADLINES_FILE'], 'deadlines', infer_schema_length=0)  # read_excel does not have infer_schema
+            .cast({pl.Null: pl.String})
+        )
         updated_deadlines = (
             deadlines
             .join(dds, on='Pharmacy License Number', how='semi')
@@ -249,14 +264,27 @@ def pharm_clean(dds: pl.LazyFrame) -> None:
         send_notices(updated_deadlines, 'friday')
 
         if today.weekday() == WEDNESDAY:  # notify compliance team of deadlines that fall in the next week
-            due_next_week = date_in_next_week(updated_deadlines)
-            # TODO: email compliance team with due_next_week
+            due_next_week = date_in_next_week(updated_deadlines).collect()
+            dnw_path = Path('deadline_next_week.csv')
+            due_next_week.write_csv(dnw_path)
+            dnw_msg = email.EmailMessage(
+                sender=os.environ['EMAIL_COMPLIANCE'],
+                to=os.environ['EMAIL_COMPLIANCE'],
+                subject='DDS Pharmacies with Deadlines Next Week',
+                message_text=f'There are {due_next_week.height} pharmacies with deadlines next week.\n{"Please see the attachment for details.\n" if due_next_week.height > 0 else "\n"}Thank you.',
+                file_paths=[dnw_path] if due_next_week.height > 0 else [],
+            )
+            email.send_email(dnw_msg, draft=(not args.send_emails))
+            dnw_path.unlink()
 
     if today.weekday() != FRIDAY:
         send_notices(dds, 'daily')
 
 
 if __name__ == '__main__':
+    parser = argparse.ArgumentParser(description='delinquent data submitters')
+    parser.add_argument('-s', '--send-emails', action='store_true', help='send emails instead of creating drafts')
+    args = parser.parse_args()
     load_dotenv()
     mp_path = Path('data/pharmacies.csv')
     files.warn_file_age(mp_path)
